45514b5a43e4fb7a892289b33b9e65073173c568
teitonny <tonnytei4@gmail.com>
2023-03-23 13:15:49 +0300

    added complete project

---
+++ src/features/cart/cartSlice.js
@@ -0,0 +1,18 @@
+import {createSlice} from "@reduxjs/toolkit";
+
+const initialState =  {
+    cartItem: [],
+    amount: 0,
+    total: 0,
+    isLoading: true,
+}
+
+
+const cartSlice = createSlice( {
+    name: 'cart',
+    initialState
+})
+
+// console.log(cartSlice);
+
+export default cartSlice.reducer;
